db8fbf1011e6042c1b196f8acc85550b4c23d0d2
Jocalu <jocalu87@gmail.com>
2021-04-05 11:05:00 +0200

    Add formcontrol contact

--- portfolio-cv/src/pages/contact/Contact.jsx
+++ portfolio-cv/src/pages/contact/Contact.jsx
@@ -3,9 +3,9 @@ import './Contact.scss';
 import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
 import MailOutlined from '@material-ui/icons/MailOutlined';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import {
+/* import {
   FormControl, InputLabel, Input, FormHelperText,
-} from '@material-ui/core';
+} from '@material-ui/core'; */
 import information from '../../constants/information-jose';
 
 export default function Contact() {
@@ -52,7 +52,25 @@ export default function Contact() {
 
         <h3>How I can help you?</h3>
 
-        <FormControl>
+        <div className="formcontrol">
+          <div className="inputs">
+            <label htmlFor="fullname">
+              <input type="text" name="fullname" className="fullname" placeholder="Full Name" />
+            </label>
+            <label htmlFor="email-address">
+              <input type="text" name="email-address" className="email-address" placeholder="Email Address" />
+            </label>
+            <label htmlFor="subject">
+              <input type="text" name="subject" className="subject" placeholder="Subject" />
+            </label>
+          </div>
+          <label htmlFor="message">
+            <textarea name="message" id="message" cols="30" rows="10" placeholder="Message" />
+          </label>
+        </div>
+        <input type="submit" className="btn btn--blue" value="Send message" />
+
+        {/*    <FormControl>
           <InputLabel htmlFor="my-input">Email address</InputLabel>
           <Input id="my-input" aria-describedby="my-helper-text" />
           <Input id="my-input" aria-describedby="my-helper-text" />
@@ -61,7 +79,7 @@ export default function Contact() {
 
           <input type="submit" className="btn btn--blue" value="Send message" />
         </FormControl>
-
+ */}
       </section>
     </main>
 
