fc5378233cb50a06b6c02b13e2b93fb46dce274a
Jocalu <jocalu87@gmail.com>
2021-04-09 18:22:18 +0200

    Add ES/EN button

--- portfolio-cv/src/components/header/Header.jsx
+++ portfolio-cv/src/components/header/Header.jsx
@@ -8,73 +8,79 @@ import { Link } from 'react-router-dom';
 
 export default function Header({ information }) {
   return (
-    <header
-      data-aos="fade-right"
-      data-aos-offset="0"
-      data-aos-easing="ease-in-sine"
-      data-aos-duration="1200"
-      data-aos-delay="2500"
-    >
-      <div className="profile">
-        <img src={information.personal.photoURL} alt={information.personal.name} />
-        <p className="name">{information.personal.name}</p>
-        <p className="profession">{information.personal.profession}</p>
-        <p className="profile__contact">{information.personal.email}</p>
-        <p className="profile__contact">{information.personal.phone}</p>
-        <div className="media">
-          <a
-            href={information.personal.linkedin}
-            className="media__icon"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <LinkedInIcon />
-          </a>
-          <a
-            href={information.personal.github}
-            className="media__icon"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <GitHubIcon />
-          </a>
+    <>
+      <div className="language">
+        <button type="button">ES</button>
+        /
+        <button type="button">EN</button>
+      </div>
+      <header
+        data-aos="fade-right"
+        data-aos-offset="0"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="1200"
+        data-aos-delay="2500"
+      >
+        <div className="profile">
+          <img src={information.personal.photoURL} alt={information.personal.name} />
+          <p className="name">{information.personal.name}</p>
+          <p className="profession">{information.personal.profession}</p>
+          <p className="profile__contact">{information.personal.email}</p>
+          <p className="profile__contact">{information.personal.phone}</p>
+          <div className="media">
+            <a
+              href={information.personal.linkedin}
+              className="media__icon"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LinkedInIcon />
+            </a>
+            <a
+              href={information.personal.github}
+              className="media__icon"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubIcon />
+            </a>
+          </div>
+
         </div>
 
-      </div>
+        <nav>
+          <Link to="/about-me" className="link">
+            <hr />
+            {information.menu.page1}
+            <ChevronRightIcon className="icon" />
+          </Link>
 
-      <nav>
-        <Link to="/about-me" className="link">
-          <hr />
-          {information.menu.page1}
-          <ChevronRightIcon className="icon" />
-        </Link>
+          <Link to="/resume" className="link">
+            <hr />
+            {information.menu.page2}
+            <ChevronRightIcon className="icon" />
+          </Link>
 
-        <Link to="/resume" className="link">
-          <hr />
-          {information.menu.page2}
-          <ChevronRightIcon className="icon" />
-        </Link>
+          <Link to="/formation" className="link">
+            <hr className="short" />
+            {`${information.menu.page3} & ${information.menu.page4}`}
+            <ChevronRightIcon className="icon" />
+          </Link>
 
-        <Link to="/formation" className="link">
-          <hr className="short" />
-          {`${information.menu.page3} & ${information.menu.page4}`}
-          <ChevronRightIcon className="icon" />
-        </Link>
+          <Link to="/projects" className="link">
+            <hr />
+            {information.menu.page5}
+            <ChevronRightIcon className="icon" />
+          </Link>
 
-        <Link to="/projects" className="link">
-          <hr />
-          {information.menu.page5}
-          <ChevronRightIcon className="icon" />
-        </Link>
-
-        <Link to="/contact" className="link">
-          <hr />
-          {information.menu.page6}
-          <ChevronRightIcon className="icon" />
-        </Link>
-      </nav>
-    </header>
-
+          <Link to="/contact" className="link">
+            <hr />
+            {information.menu.page6}
+            <ChevronRightIcon className="icon" />
+          </Link>
+        </nav>
+      </header>
+    </>
   );
 }
 
